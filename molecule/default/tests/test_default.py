1378ee63db4811c6c8f51d99b454dcd3e7f1a63c
darkwizard242 <am900820@gmail.com>
2022-08-05 09:06:43 -0400

    change PACKAGE_BINARY value in molecule testinfra

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,7 +7,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 PACKAGE = 'grype'
-PACKAGE_BINARY = "/usr/local/bin/grype"
+PACKAGE_BINARY = "/usr/local/grype"
 
 
 def test_grype_package_installed(host):
